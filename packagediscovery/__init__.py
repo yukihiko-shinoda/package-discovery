aa54eef721e522cbb942bb2c3e8e212295820d61
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-09-01 16:58:00 +0900

    Bump version: 0.1.2 → 0.2.0

--- packagediscovery/__init__.py
+++ packagediscovery/__init__.py
@@ -5,7 +5,7 @@ from packagediscovery.setuptools import *  # noqa: F403
 
 __author__ = """Yukihiko Shinoda"""
 __email__ = "yuk.hik.future@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __all__ = []
 __all__ += packages.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
