44d38157ad37ed3910032af1d04d2058d3e1f2cf
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-09-01 11:36:21 +0900

    Fix bug that __all__ is not defined

--- packagediscovery/__init__.py
+++ packagediscovery/__init__.py
@@ -1,5 +1,12 @@
 """Top-level package for Package Discovery."""
 
+from packagediscovery.packages import *  # noqa: F403
+from packagediscovery.setuptools import *  # noqa: F403
+
 __author__ = """Yukihiko Shinoda"""
 __email__ = "yuk.hik.future@gmail.com"
 __version__ = "0.1.0"
+
+__all__ = []
+__all__ += packages.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
+__all__ += setuptools.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
--- packagediscovery/packages.py
+++ packagediscovery/packages.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+__all__ = ["Packages"]
+
 
 @dataclass
 class PairPackage:
